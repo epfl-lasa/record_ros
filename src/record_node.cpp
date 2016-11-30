1eb83e025a511a154b7f860dca86883949920efe
Boris_il_forte <boris.ilpossente@hotmail.it>
2016-11-30 17:50:04 +0100

    Record all bags if no topic specified

--- src/record_node.cpp
+++ src/record_node.cpp
@@ -53,18 +53,26 @@ int main(int argc, char** argv)
     ROS_INFO_STREAM("file_name: "   << file_name);
 
     // Extract all topics white space seperated
-    boost::regex rgx("\\s+");
-    boost::sregex_token_iterator iter(topic.begin(), topic.end(),rgx,-1);
-    boost::sregex_token_iterator end;
-    std::vector<std::string> topics;
-    for ( ; iter != end; ++iter){
-        topics.push_back(*iter);
-        std::cout << *iter << '\n';
-    }
-
     rosbag::RecorderOptions options;
-    options.topics = topics;
+	
     options.prefix = path_save + "/" + file_name;
+
+    if(topic.empty())
+    {
+        options.record_all = true;
+    }
+    else
+    {
+        boost::regex rgx("\\s+");
+        boost::sregex_token_iterator iter(topic.begin(), topic.end(),rgx,-1);
+        boost::sregex_token_iterator end;
+        std::vector<std::string> topics;
+        for ( ; iter != end; ++iter){
+            topics.push_back(*iter);
+        }
+        
+        options.topics = topics;    
+    }
 
     Record* record = new Record(nh,options);
     //ROS_INFO_STREAM("RECORD_NODE: Waiting for callback()!");
